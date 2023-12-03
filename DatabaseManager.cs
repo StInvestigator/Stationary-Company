43ecbdb4b2f2e3e1e25fc77d5fc047c835c8a8ef
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-12-03 14:00:18 +0200

    Add project files.

---
+++ DatabaseManager.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stationary_Company
+{
+    public class DatabaseManager
+    {
+        public string ConnectionString { get; set; }
+
+        private SqlConnection sqlConnection { get; set; }
+
+        public DatabaseManager(string connectionString)
+        {
+            ConnectionString = connectionString;
+            sqlConnection = new SqlConnection(ConnectionString);
+        }
+        public bool OpenConnection()
+        {
+            try
+            {
+                sqlConnection.Open();
+                return true;
+            }
+            catch (SqlException sql_ex)
+            {
+                throw new Exception(sql_ex.Message);
+            }
+        }
+        public bool CloseConnection()
+        {
+            try
+            {
+                sqlConnection.Close();
+                return true;
+            }
+            catch (SqlException sql_ex)
+            {
+                throw new Exception(sql_ex.Message);
+            }
+        }
+        public void ExecuteNonQuery(string query)
+        {
+            using (SqlConnection _sqlConnection = new SqlConnection(ConnectionString))
+            {
+                if (_sqlConnection.State == System.Data.ConnectionState.Closed)
+                {
+                    _sqlConnection.OpenAsync();
+                }
+                SqlCommand sqlCommand = new SqlCommand(query, _sqlConnection);
+                sqlCommand.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.Data.SqlClient;
+using Stationary_Company;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseManager database = new DatabaseManager(@"Data Source=DESKTOP-OF66R01\\SQLEXPRESS;Initial Catalog=StationeryCompany;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        }
+    }
+}
